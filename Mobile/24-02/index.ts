6ec3f9813d98a4bfd50dd1c325ba884116771987
Cassio <cassioe.valezzi@gmail.com>
2025-03-17 23:16:28 -0300

    feat: Exercício 2 - Função Tipada

--- Mobile/24-02/index.ts
+++ Mobile/24-02/index.ts
@@ -1,3 +1,12 @@
+//Exercício 1 - Tipagem e Arrays
+
 let valores: number[] = [10, 20, 30];
 let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
 console.log("Soma dos valores:", somaValores);
+
+//Exercício 2 - Função Tipada
+
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));

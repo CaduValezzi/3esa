508cfd412d7e263b4ca908c96fc2948354e670b0
Cassio <cassioe.valezzi@gmail.com>
2025-03-17 23:29:56 -0300

    feat(exer 2): interface de multiplicacao

--- Mobile/06-03/index.ts
+++ Mobile/06-03/index.ts
@@ -14,3 +14,12 @@ const carro1: Carro = {
 };
 
 console.log("Carro 1:", carro1);
+
+//Exercício 2 - Interface para Multiplicação 
+interface Multiplicacao {
+    (a: number, b: number): number;
+}
+
+const multiplicar: Multiplicacao = (x, y) => x * y;
+console.log("Multiplicação 2 x 8:", multiplicar(2, 8));
+console.log("Multiplicação 3 x 4:", multiplicar(3, 4)); 

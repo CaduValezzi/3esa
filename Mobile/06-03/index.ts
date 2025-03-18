6694ad4f65178dc306ae5e68f2c782460e0f6731
Cassio <cassioe.valezzi@gmail.com>
2025-03-17 23:31:32 -0300

    feat(exer 3): Função Genérica inverterArray<T>

--- Mobile/06-03/index.ts
+++ Mobile/06-03/index.ts
@@ -23,3 +23,13 @@ interface Multiplicacao {
 const multiplicar: Multiplicacao = (x, y) => x * y;
 console.log("Multiplicação 2 x 8:", multiplicar(2, 8));
 console.log("Multiplicação 3 x 4:", multiplicar(3, 4)); 
+
+//Exercício 3 - Função Genérica inverterArray<T>
+
+function inverterArray<T>(array: T[]): T[] {
+    return array.reverse();
+}
+const numeros = [1, 2, 3, 4, 5];
+const palavras = ["isso", "é", "um", "teste"];
+console.log("Array invertido (números):", inverterArray(numeros)); 
+console.log("Array invertido (palavras):", inverterArray(palavras)); 
